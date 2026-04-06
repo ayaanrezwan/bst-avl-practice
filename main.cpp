dcebc9c4e71e1209acee8c1cc0981291dd5db7c5
ayaanrezwan <arezwan9460@outlook.com>
2026-04-06 00:24:51 -0400

    Implemented tree class and printed outputs in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    // Creating a tree and assigning values
+    TreeNode<int>* root = new TreeNode<int>(60);
+    root -> left = new TreeNode<int>(55);
+    root -> right = new TreeNode<int>(100);
+
+    // Printing root (parent) element
+    cout << root -> element << endl;
+
+    // Printing root left child element
+    cout << root -> left -> element << endl;
+
+    // Printing root right child element
+    cout << root -> right -> element << endl;
 
     return 0;
 }
